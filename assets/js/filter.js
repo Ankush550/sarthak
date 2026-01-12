c496014556ae8206b1fd82dfc1bbeed9cf4643ff
Ankush Jain <ankush.jain70146@gmail.com>
2026-01-12 21:58:02 +0530

    fix base path for localhost and github pages

--- assets/js/filter.js
+++ assets/js/filter.js
@@ -13,17 +13,4 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
-document.addEventListener("DOMContentLoaded", function () {
-  const searchInput = document.getElementById("schemeSearch");
-  const cards = document.querySelectorAll(".scheme-card");
-
-  searchInput.addEventListener("keyup", function () {
-    const value = searchInput.value.toLowerCase();
-
-    cards.forEach(card => {
-      const text = card.innerText.toLowerCase();
-      card.style.display = text.includes(value) ? "block" : "none";
-    });
-  });
-});
 
